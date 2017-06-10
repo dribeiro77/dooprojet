6bdc50984cfe2866913e750c08e0df19fd9d2993
Vic206 <vicnesh@hotmail.fr>
2017-06-10 12:59:28 +0200

    Add files via upload

---
+++ ChoixPartieUI.java
@@ -0,0 +1,146 @@
+package vue;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
+
+import javax.swing.*;
+
+import controller.ChoixPartieController;
+import controller.JoueDamesChinoisesController;
+import domaine.Joueur;
+
+public class ChoixPartieUI extends JFrame implements ActionListener{
+	private ChoixPartieController control;
+
+	
+	public ChoixPartieUI(){
+		super("Choix Partie");
+		control = new ChoixPartieController();
+		setSize(850,650);
+		setVisible(true);
+		setLocationRelativeTo(null);
+		//add(new JLabel(new ImageIcon("fondchoix.jpeg")));
+		draw();
+		revalidate();
+		
+	}
+	
+	public void draw(){
+		JPanel left = new JPanel();
+		JPanel right = new JPanel();
+		JPanel flow = new JPanel();
+
+
+		this.setLayout(new GridLayout(1, 2));
+		//left.add(flow);
+
+		//JSeparator separate = new JSeparator(SwingConstants.VERTICAL);
+
+		// LEFT : Dames Chinoises
+		flow.setLayout(new FlowLayout());
+		left.setLayout(new GridLayout(3, 1));
+		left.setBorder(BorderFactory.createLineBorder(Color.white, 10));
+		right.setBorder(BorderFactory.createLineBorder(Color.white,10));
+
+		// CASE N°1 :
+		flow = new JPanel();
+		flow.setLayout(new FlowLayout());
+
+		JLabel dames = new JLabel("DAMES CHINOISES");
+		dames.setFont(new Font("Batang",20,40));
+		// Test
+		//dames.setVerticalAlignment(SwingConstants.CENTER);
+		flow.add(dames);
+		//
+		flow.setBackground(Color.lightGray);
+		left.add(flow);
+
+		// CASE N°2 :
+		flow = new JPanel();
+		flow.setLayout(new FlowLayout());
+		ImageIcon icone = new ImageIcon("DamesChinoises.png");
+		JLabel image = new JLabel(icone);
+        left.add(image);
+		left.setBackground(Color.lightGray);
+
+		// CASE N°3 :
+		flow = new JPanel();
+		flow.setLayout(new FlowLayout());
+
+		JButton play = new JButton( new AbstractAction("Reglage Dames Chinoises") {
+			@Override
+			public void actionPerformed( ActionEvent e ) {
+				control.choixJeu("Dames Chinoise");
+				if(control.lancePartie()){
+					dispose();
+					ReglageDamesChinoiseUI reglage = new ReglageDamesChinoiseUI();
+				}
+				else {
+					JOptionPane.showMessageDialog(null, "Ne peut pas lancer la partie", "Erreur", JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		});
+		flow.add(play);
+		flow.setBackground(Color.lightGray);
+		left.add(flow);
+		this.add(left);
+
+
+		/*__________________________________________________________________________________________________________
+		__________________________________________________________________________________________________________*/
+
+
+		// RIGHT : Abalone
+
+		// CASE N°1 : Nom
+		flow = new JPanel();
+		flow.setLayout(new FlowLayout());
+		right.setLayout(new GridLayout(3, 1));
+
+		JLabel abalone = new JLabel("ABALONE",JLabel.CENTER);
+		abalone.setFont(new Font("Batang",20,40));
+		flow.add(abalone,BorderLayout.CENTER);
+		flow.setBackground(Color.lightGray);
+		right.add(flow);
+
+		// CASE N°2 : Image
+        flow = new JPanel();
+        flow.setLayout(new FlowLayout());
+        ImageIcon icone2 = new ImageIcon("Abalone.png");
+        JLabel image2 = new JLabel(icone2);
+        right.add(image2);
+        right.setBackground(Color.lightGray);
+
+		// CASE N°3 : Bouttons
+		flow = new JPanel();
+		flow.setLayout(new FlowLayout());
+
+		JButton playAba = new JButton( new AbstractAction("Réglage Abalone") {
+			@Override
+			public void actionPerformed( ActionEvent e ) {
+				control.choixJeu("Abalone");
+				if(control.lancePartie()){
+					dispose();
+					ReglageAbalone reg2 = new ReglageAbalone();
+				}
+				else {
+					JOptionPane.showMessageDialog(null, "Ne peut pas lancer la partie", "Erreur", JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		});
+		flow.add(playAba);
+		flow.setBackground(Color.lightGray);
+		right.add(flow);
+
+		this.add(right);
+
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+	}
+}
---
+++ JoueDamesChinoisesUI.java
@@ -0,0 +1,168 @@
+package vue;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JSeparator;
+
+import controller.JoueDamesChinoisesController;
+import domaine.Case;
+import domaine.PlateauDamesChinoises;
+
+public class JoueDamesChinoisesUI extends JFrame implements ActionListener  {
+	private JoueDamesChinoisesController control;
+	RoundButton cases[] = new RoundButton[122];
+	//JPanel plat = new JPanel();
+	RoundButton selected = new RoundButton(new Case(0));
+	
+	public JoueDamesChinoisesUI(JoueDamesChinoisesController c){
+		super("Dammes Chinoises");
+		control=c;
+		setSize(1000, 700);
+		setVisible(true);
+		setLocationRelativeTo(null);
+		draw();
+		//this.add(plat);
+		this.revalidate();
+	}
+	
+	public void draw(){
+
+
+		PlateauDamesChinoises plateau = (PlateauDamesChinoises) control.getPartie().getPlateau();
+		for(int i=1; i<=121; i++){
+			cases[i]= new RoundButton(plateau.getPlateau()[i]);
+            
+		}
+
+		this.setLayout(new GridLayout(18, 3));
+
+		JPanel one = new JPanel();
+		cases[1].addActionListener(this);
+		one.add(cases[1]);
+		this.add(one);
+		
+		one = new JPanel();
+		cases[2].addActionListener(this);
+		one.add(cases[2]);
+		cases[3].addActionListener(this);
+		one.add(cases[3]);
+		this.add(one);
+		
+		addBetween(4, 6);
+		addBetween(7, 10);
+		addBetween(11, 23);
+		addBetween(24, 35);
+		addBetween(36, 46);
+		addBetween(47, 56);
+		addBetween(57, 65);
+		addBetween(66, 75);
+		addBetween(76, 86);
+		addBetween(87, 98);
+		addBetween(99, 111);
+		addBetween(112, 115);
+		addBetween(116, 118);
+		addBetween(119, 120);
+
+		one = new JPanel();
+		cases[121].addActionListener(this);
+		one.add(cases[121]);
+		this.add(one);
+		
+		one = new JPanel();
+		JButton quit = new JButton("Quitter");
+		quit.addActionListener(this);
+		one.add(quit, BorderLayout.EAST);
+		this.add(one, BorderLayout.EAST);
+		
+        this.setVisible(true);
+    }
+
+	/**
+	 * Ajoute les boutons ronds sur la JFrame
+	 * @param min
+	 * @param max
+	 */
+	public void addBetween(int min, int max){
+		JPanel one = new JPanel();
+		for (int i = min; i <= max ; i++) {
+			cases[i].addActionListener(this);
+			one.add(cases[i]);
+		}
+		this.add(one);
+	}
+
+	/**
+	 * Actions quand on clique sur une case
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		if(e.getSource().getClass().equals(JButton.class)){
+			dispose();
+		}
+		else{
+			
+			System.out.println(((RoundButton)e.getSource()).getCases().getId());
+		
+			
+		//decolore les mouvements pass�s 
+		if (((RoundButton)e.getSource()).getCases().getPion()!=null){
+			ArrayList<Case> res = control.mouvements_possibles(selected.getCases());		
+		for (int i = 0; i < res.size(); i++) {
+			cases[res.get(i).getId()].setForeground(Color.GRAY);
+			}
+			selected.setForeground(Color.GRAY);
+		}
+		
+		
+		//si il selectione un boutton color�
+		if(((RoundButton)e.getSource()).getForeground()==Color.PINK){
+			if(control.getPartie().getPlateau().estVoisin(((RoundButton)e.getSource()).getCases(),selected.getCases())){
+				control.deplacementSimple(selected.getCases(), ((RoundButton)e.getSource()).getCases());
+			}
+			else {
+				control.sautePion(selected.getCases(), ((RoundButton)e.getSource()).getCases());
+			}
+			
+			//decolore les mouvements pass�s 
+			if (((RoundButton)e.getSource()).getCases().getPion()!=null){
+				ArrayList<Case> res = control.mouvements_possibles(selected.getCases());		
+			for (int i = 0; i < res.size(); i++) {
+				cases[res.get(i).getId()].setForeground(Color.GRAY);
+				}
+			}
+			
+			
+		}
+		
+		
+	
+		//collore mouvements possibles
+		if (((RoundButton)e.getSource()).getCases().getPion()!=null){
+			ArrayList<Case> res = control.mouvements_possibles(((RoundButton)e.getSource()).getCases());
+			for (int i = 0; i < res.size(); i++) {
+				cases[res.get(i).getId()].setForeground(Color.PINK);
+			}
+		}
+		
+		
+		selected=((RoundButton)e.getSource());
+		
+		if(control.partie_finie()){
+			JOptionPane.showMessageDialog(this, "PARTIE FINIE!!");
+			dispose();
+		}
+		}
+	}
+}
