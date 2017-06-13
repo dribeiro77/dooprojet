ab26d6c486d57dbcb4bea7b3acfeba3f382c28df
kharraz <kharraz@kharraz-Lenovo-IdeaPad-Z510>
2017-06-13 18:46:33 +0200

    JTable : Classement joueurs + Affichage

---
+++ doo-projet/src/domaine/Main.java
@@ -0,0 +1,88 @@
+package domaine;
+
+import java.awt.BorderLayout;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+import domaine.CRUD;
+import domaine.Joueur;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		CRUD a = new CRUD();
+		Joueur J = new Joueur("Karim");
+		try {
+			a.connection();
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			a.ajouter_Joueur(J);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			
+			a.scoreAbalone(J, 2);
+		} catch (SQLException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+	
+		ResultSet res = null;
+		
+		try {
+			res = a.lister_joueur();
+			
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		
+		try {
+			a.afficher(res);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+	    
+ /* JTable */
+		
+		ResultSet rs = null;
+		
+		try {
+			rs = a.lister_joueur_abalone();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    ResultSetTableModel model = new ResultSetTableModel( rs );
+	    JTable table = new JTable(model);
+	    JScrollPane Jscroll = new JScrollPane(table);
+        
+        JFrame mainFrame = new JFrame( "Affiche table " );
+        mainFrame.add( Jscroll, BorderLayout.CENTER );
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE );
+        mainFrame.setSize( 640, 480 );
+        mainFrame.setVisible( true );
+		
+	}
+		
+		
+
+}
