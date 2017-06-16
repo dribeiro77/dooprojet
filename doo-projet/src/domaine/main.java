e0918b28088c7f3a512dc1d9202be246bcbc6a88
Vic206 <vicnesh@hotmail.fr>
2017-06-16 15:02:40 +0200

    main

--- doo-projet/src/domaine/main.java
+++ doo-projet/src/domaine/main.java
@@ -6,7 +6,7 @@ import controller.JoueDamesChinoisesController;
 import vue.ChoixPartieUI;
 import vue.JoueDamesChinoisesUI;
 
-public class main {
+class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
